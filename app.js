a7bbffe0da423130d22cf8e6b5eaef42dd1ae3f2
data-amit-kumar <data2.amitkumar@gmail.com>
2024-01-02 01:40:58 +0530

    change 2

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const http = require('http');
 
 const server= http.createServer((req,res)=>{
-    console.log("Amit kumar");
-    process.exit();
+    const url= req.url;
+    if(url==="/"){
+        res.write('<html>');
+        res.write('<head><title>My First page</title></head>');
+        res.write('<body><h1>Hello</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(url==='/home'){
+        res.write('<html>');
+        res.write('<head><title>My First page</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(url==='/about'){
+        res.write('<html>');
+        res.write('<head><title>My First page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(url==='/node'){
+        res.write('<html>');
+        res.write('<head><title>My First page</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+
+    
 });
 
 server.listen(4000)
