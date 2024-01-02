f649f08690e5cefc8f24c649de4f809a7e556867
data-amit-kumar <data2.amitkumar@gmail.com>
2024-01-03 05:26:12 +0530

    change6

--- app1.js
+++ app1.js
@@ -1,5 +1,17 @@
 const http= require('http');
-const route=require('./routes');
 
-const server= http.createServer(route.handle);
+const express= require('express');
+const app= express();
+
+app.use((req,res,next)=>{
+    console.log("Hi there")
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("Hi Again");
+    res.send("<h1>Hello Brother</h1>");
+});
+
+const server= http.createServer(app);
 server.listen(3000);
