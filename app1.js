e1dc62c27f1a3155fe6bb0283d1220681d6112c8
data-amit-kumar <data2.amitkumar@gmail.com>
2024-01-03 22:00:30 +0530

    change 7

--- app1.js
+++ app1.js
@@ -2,14 +2,20 @@
 
 const express= require('express');
 const app= express();
+const bodyparser=require('body-parser');
 
-app.use((req,res,next)=>{
-    console.log("Hi there")
-    next();
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">ADD PRODUCT</button></form>')
+});
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use((req,res,next)=>{
-    console.log("Hi Again");
     res.send("<h1>Hello Brother</h1>");
 });
 
