5738820c743d37609c259789fe979cd7ac4ec833
data-amit-kumar <data2.amitkumar@gmail.com>
2024-01-03 22:31:43 +0530

    change 8

--- app1.js
+++ app1.js
@@ -2,21 +2,21 @@
 
 const express= require('express');
 const app= express();
+
+const adminRoutes= require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 const bodyparser=require('body-parser');
 
 app.use(bodyparser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">ADD PRODUCT</button></form>')
-});
-
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.send("<h1>Hello Brother</h1>");
-});
+    res.status(404).send('<h1>Page Not found</h1>');
+})
+
+
 
 app.listen(3000)
